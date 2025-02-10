4e74847a120acb1a87033817b46da39757d63bb0
Thea Ganza <s2200535@usls.edu.ph>
2025-02-10 09:36:14 +0800

    Draft

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function index() {
-        return view("logIn.index");
+        return view("login.index");
     }
 }
