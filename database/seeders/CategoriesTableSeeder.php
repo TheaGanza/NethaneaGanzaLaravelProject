8541f14460e3258565ac3587f753e744ece5dc65
Thea Ganza <s2200535@usls.edu.ph>
2025-03-10 09:09:26 +0800

    Models models models

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -9,12 +9,12 @@ class CategoriesTableSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('categories')->insert([
+        /* DB::table('categories')->insert([
             ['id' => 1, 'name' => 'Pastry', 'description' => 'Sweet and savoury baked goods','created_at'=> now(),'updated_at'=> now()],
             ['id' => 2, 'name' => 'Candy', 'description' => 'Confections made with sugar.','created_at'=> now(),'updated_at'=> now()],
             ['id' => 3, 'name' => 'Pasta', 'description' => 'Resources for students and teachers.','created_at'=> now(),'updated_at'=> now()],
             ['id' => 4, 'name' => 'Pizza', 'description' => 'Flattened disk of bread dough with toppings baked quickly.','created_at'=> now(),'updated_at'=> now()],
             ['id' => 5, 'name' => 'Beverage', 'description' => 'Drinks other than water.','created_at'=> now(),'updated_at'=> now()],
-        ]);
+        ]); */
     }
 }
