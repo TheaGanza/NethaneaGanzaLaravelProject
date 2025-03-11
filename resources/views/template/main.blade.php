102ccc10d06ae099a4b5814039c5315a93193b20
Thea Ganza <s2200535@usls.edu.ph>
2025-03-11 23:17:46 +0800

    Displaying categories data with using laravel directives

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', 'Laravel App')</title>
+    <title>@yield('title')</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,11 @@ Route::get('/', function () {
 });
 
 Route::get('/login', 'App\Http\Controllers\LoginController@index');
+
+Route::get('/categories', 'App\Http\Controllers\CategoriesController@index');
+
 Route::get('/create', 'App\Http\Controllers\InventoryController@create');
+
 Route::get('/inventory', 'App\Http\Controllers\InventoryController@index');
+
 Route::get('/hello', 'App\Http\Controllers\HelloController@index');
